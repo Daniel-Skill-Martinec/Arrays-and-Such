8810e32f59d2634b728fd4118256de21bb3bacd2
Daniel-Skill-Martinec <danielpokecraft@gmail.com>
2022-05-01 23:07:04 -0400

    Add project files.

---
+++ Arrays and Such/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays_and_Such
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The List
+            string[] Pokemon = { "Pikachu", "Ninjask", "Palkia", "Jolteon", "Rhydon"};
+
+            //Change the name of the thing you want
+            Pokemon[1] = "Shedinja";
+
+            //Sorts in Alphbetical Order with ForEach
+            Array.Sort(Pokemon);
+            foreach (string i in Pokemon)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Same can be done with Numbers
+            int[] myNum = { 10, 13, 17, 50, 67, 89 };
+            Console.WriteLine(myNum.Max()); //Largest
+            Console.WriteLine(myNum.Min()); //Smallest
+            Console.WriteLine(myNum.Sum()); //Sum of the All
+
+        }
+    }
+}
